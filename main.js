641e9db3e670375c159675c21f0a2f0f0b7f373c
Oliver Szabo <oliverszbo@gmail.com>
2020-12-10 09:31:04 +0100

    project scope-ja

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 function _load () {
     //to-do app
     //10 üres mező van alapértelmezésben checkbox, text-in, remove
-
+    //rootarray[9]
+    //for ciklus -> 10 div checkbox, input, remove button
+    //add button a végére insertadjacenthtml -> afterend
+    //add buttonhoz addeventlistener click-re -> új elem a sorhoz
+    //remove button -> queryselectorall 
+    //for ciklus -> mindegyikre click eseményt adunk
 }
 window.addEventListener("load", _load);
